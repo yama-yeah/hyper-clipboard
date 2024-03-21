de1baca53908eef9515f12c8668b406bf51c2543
yama-yeah <b1021091@fun.ac.jp>
2024-03-21 12:40:39 +0900

    add const values file

---
+++ src/hyper_clipboard/const/const_values.py
@@ -0,0 +1,34 @@
+from socket import gethostname
+# pyright: reportPrivateImportUsage=false
+from sqids import Sqids
+hostname = gethostname()
+squids=Sqids(blocklist=[])
+from dataclasses import dataclass
+from enum import Enum
+@dataclass
+class UUIDNames:
+    MAIN="A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+    TIMESTAMP="120615a4-aaa6-481f-bcdc-5e222bf3551d"
+    CLIPBOARD="c96fec7b-c13e-44d3-8291-9408db876f27"
+    CLIPBOARD_ID="c2eef872-8779-474c-9361-f2db13cf1d7b"
+    UPDATING_STATE="b4d0dc3d-8474-4810-a873-97387cd18d35"
+    @staticmethod
+    def get_name_from_uuid(uuid:str)->str:
+        for name in UUIDNames.__dict__:
+            if UUIDNames.__dict__[name]==uuid:
+                return name
+        raise ValueError(f"uuid:{uuid} is not found in UUIDNames")
+@dataclass
+class UPDATING_STATE:
+    NOT_UPDATING=b"0"
+    UPDATING=b"1"
+    UPDATED=b"2"
+@dataclass
+class ObserverNames:
+    CLIP="clip"
+    BLE="ble"
+
+class AppEvents(Enum):
+    NEUTRAL= "neutral"
+    CLIPBOARD_CHANGED = "clipboard_changed"
+    BLE_CLIP_RECEIVED = "ble_clip_received"
