a6013c9c28e374799ec9fc41ce2896f3a13d1116
yama-yeah <b1021091@fun.ac.jp>
2024-04-04 06:42:27 +0900

    Fix asynchronous retrieved values to verify if they are really optimal

--- src/hyper_clipboard/__init__.py
+++ src/hyper_clipboard/__init__.py
@@ -1,14 +1,14 @@
-from hyper_clipboard.const.observable_objects import Observable, ObservableAsyncStreamer
+from hyper_clipboard.const.observable_objects import Observable, ObservableAsyncStreamer, ObservableStreamer
 from hyper_clipboard.const.storage_settings import AppConfigKeys, AppConfigStorageSettings
 from hyper_clipboard.const.utils import get_config_path
-from hyper_clipboard.services.bluetooth.bt_base_objects import BTObjectObservable
+from hyper_clipboard.services.bluetooth.bt_base_objects import BTObjectObservable, BTObjectState
 from hyper_clipboard.services.bluetooth.bt_client import BTClients
 from hyper_clipboard.services.bluetooth.bt_device_manager import BTDevicesManager
 from hyper_clipboard.services.bluetooth.bt_discover import DiscoverObservable
 from hyper_clipboard.services.clipboard.clip_manager import ClipLogsManager
 from hyper_clipboard.services.clipboard.clip_watcher import ClipObservable
 from hyper_clipboard.services.bluetooth.bt_server import BTServer, InputBTObjectState
-from hyper_clipboard.const.const_values import AppBTMode, AppLogger, ObserverNames
+from hyper_clipboard.const.const_values import UPDATING_STATE, AppBTMode, AppLogger, ObserverNames
 import asyncio
 import nest_asyncio
 
@@ -18,6 +18,7 @@ from hyper_clipboard.services.storage.shared import SharedJsonStorage
 from hyper_clipboard.services.storage.storage_base import StorageIOCombine, StorageIODataPath, StorageObservable
 nest_asyncio.apply()
 
+import pyperclip
 def _main():
     main_loop=asyncio.get_event_loop()
     app_config_storage=SharedJsonStorage(AppConfigStorageSettings(),get_config_path(app_name="hyper_clipboard"))
@@ -52,21 +53,24 @@ def _main():
     observers_dict[ObserverNames.STORAGE]=storage_observable
     
     observer_list=list(observers_dict.values())
-    stream=ObservableAsyncStreamer(observer_list,2,main_loop).sink()
+    stream=ObservableAsyncStreamer(observer_list,0.1,main_loop).sink()
     
     for data in stream:
-        
+        AppLogger.info(data.data,header_text=data.from_)
         match data.from_:
             case ObserverNames.CLIP:
-                AppLogger.info(data.data,header_text=f"stream:{data.from_}")
-                if mode==AppBTMode.CLIENT:
-                    bt_clients.update_server_from_input(InputBTObjectState.from_object_log(data.data))
-                elif mode==AppBTMode.SERVER:
-                    server.update_server_from_input(InputBTObjectState.from_object_log(data.data))
+                if pyperclip.paste()==data.data.value:
+                    if mode==AppBTMode.CLIENT:
+                        bt_clients.update_server_from_input(InputBTObjectState.from_object_log(data.data))
+                    elif mode==AppBTMode.SERVER:
+                        server.update_server_from_input(InputBTObjectState.from_object_log(data.data))
             case ObserverNames.BLE_DISCOVERER:
                 bt_clients.connect()
             case ObserverNames.BLE_SERVER:
-                clip_logs_manager.add_log(data.data)
+                bt_object_state:BTObjectState=data.data # type: ignore
+                if bt_object_state.updating_state!=UPDATING_STATE.UPDATING:
+                    AppLogger.debug(bt_object_state.time_stamp,header_text="BTMain")
+                    clip_logs_manager.add_log(data.data)
             case ObserverNames.BLE_CLIENT:
                 clip_logs_manager.add_log(data.data)
             case ObserverNames.STORAGE:
@@ -74,11 +78,11 @@ def _main():
                 clip_observable.interval=app_config[AppConfigKeys.CLIPBOARD_INTERVAL]
                 if mode==AppBTMode.CLIENT:
                     clients_state_observable.interval=app_config[AppConfigKeys.BT_CLIENTS_INTERVAL]
+                    devices_discover_observable.interval=app_config[AppConfigKeys.BT_DISCOVER_INTERVAL]
+                    devices_manager.allow_devices=app_config[AppConfigKeys.ALLOWED_DEVICES]
                 elif mode==AppBTMode.SERVER:
                     server_state_observable.interval=app_config[AppConfigKeys.BT_SERVER_INTERVAL]
                 
-                devices_discover_observable.interval=app_config[AppConfigKeys.BT_DISCOVER_INTERVAL]
-                devices_manager.allow_devices=app_config[AppConfigKeys.ALLOWED_DEVICES]
         
 
 def main():
